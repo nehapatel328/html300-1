1c6702304c39b5c48bc77ae76bece7ffd05d3549
Neha Patel <nehapatel0393@gmail.com>
2019-08-23 22:42:23 -0500

    components for header and images

---
+++ lesson08/assignment/cli2/src/components/Lessonimages.vue
@@ -0,0 +1,18 @@
+<template>
+<div class="image">
+  <img v-for="link in album" :alt='link.alt' v-bind:src='images[0 | 1 | 2]' :class="{'img-fluid img-thumbnail'}">
+
+
+</div>
+</template>
+
+<template>
+	<div class="image_template">
+		<div class="poster" v-for="(link, index) in album" :key='index'>
+		<img  :src='link.src' :alt='link.alt' :class="{'img-thumbnail' : link.isActive}" class="img-fluid " @click="link.isActive = !link.isActive">
+		<p>{{link.title}}</p>
+		<p>{{link.info}}</p>
+	</div>
+	</div>
+
+</template>
---
+++ lesson08/assignment/cli2/src/components/images.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="image">
+    <img v-for="link in album" v-bind:src='images[0 | 1 | 2]' class="img-fluid img-thumbnail">
+  </div>
+</template>
+
+<script>
+//To import the images and header components
+import myImages from '../../components/Lessonimages.vue';
+import myHeader from '../../components/Lessonheader.vue';
+
+    export default{
+        name: 'image',
+        data () {
+            return{
+                album: [require('../assets/headshot1.jpeg'),
+                        require('../assets/headshot1.jpeg')
+                        ]
+            }
+
+        }
+        images: [
+          {
+            id:"0",
+            url:"../assets/headshot1.jpeg",
+            title:"",
+            alt:"some text",
+          },
+          {
+            id:"1",
+            url:"../assets/headshot1.jpeg",
+            title:"some text",
+            alt:"some text",
+          },
+          {
+            id:"2",
+            url:"../assets/headshot1.jpeg",
+            title:"some text",
+            alt:"some text",
+          },
+        ]
+      const image = {
+      template: '#header',
+      mixins: [showHide]
+        };
+      const Nav = {
+      template: '#nav',
+      mixins: [showHide]
+      };
+
+    //Filter for header
+      filters: {
+      caps {
+        capitalize: function (value) {
+          if (!value) return ''
+          value = value.toString()
+          return value.charAt(0).toUpperCase() + value.slice(1)
+          }
+        }
+      }
+    }
+
+
+</script>
+
+<style>
+</style>
